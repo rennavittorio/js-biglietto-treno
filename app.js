dfa42f4a64b5ba497abf4d721c001dd28d0d8b1b
rennavittorio <renna.vittorio@gmail.com>
2023-01-31 15:14:10 +0100

    ADD algorithm in human words added

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 //control
 console.log('work!')
+
+//ESERCIZIO
+
+//1. chiedere numero di chilometri
+
+//2. chiedere età del passeggero
+
+//3. calcolare il prezzo totale del viaggio, secondo queste regole:
+    // 3.1 moltiplicare numero km * 0.21 €
+
+    // 3.2 valutare età passeggero
+        // SE minorenne (età < 18), applicare 20% sconto
+        // SE anziano (età > 65), applicare 40% sconto
+        // ALTRI CASI no sconto
+
+// 4. mostrare prezzo finale biglietto (2 decimali)
