ecf42fc86a40620ad30621363cb412f964f8e219
rennavittorio <renna.vittorio@gmail.com>
2023-01-31 18:08:19 +0100

    ADD data change to html file completed

--- bonus/bonus.js
+++ bonus/bonus.js
@@ -1,7 +1,10 @@
 //control
 // console.log('work!')
 
-//ESERCIZIO
+// richiedere nome e cognome
+let nameUser = prompt('Come ti chiami');
+console.log(typeof nameUser, nameUser);
+
 
 //1. chiedere numero di chilometri e applico prima trasformazione numero in 2 decimali
 let routeDistance = parseFloat(prompt('Inserisci la distanza desiderata (in km).')).toFixed(2);
@@ -11,7 +14,7 @@ console.log(typeof routeDistance, routeDistance);
 //2. chiedere anno passeggero e calcolare età
 //2.1 richiedere anno di nascita
 let dobUser = parseInt(prompt('Inserisci l\'anno di nascita.'));
-console.log(typeof dobUser, dobUser)
+console.log(typeof dobUser, dobUser);
 
 //2.2 settare giorno corrente
 let today = new Date();
@@ -27,21 +30,53 @@ console.log(typeof ageUser, ageUser);
 const priceAtKilometer = 0.21;
 let ticketPrice = parseFloat(routeDistance * priceAtKilometer).toFixed(2);
 ticketPrice = parseFloat(ticketPrice); //trasformo var in number (workaround toFixed method)
-console.log(typeof ticketPrice, ticketPrice)
+console.log(typeof ticketPrice, ticketPrice);
+
+// inserire informazioni a schermo
+//recupero info da document
+let currentName = document.getElementById('current-name');
+let currentAge = document.getElementById('current-age');
+let currentRouteDistance = document.getElementById('current-route');
+let currentTicketPrice = document.getElementById('current-price');
+let currentDiscountType = document.getElementById('current-discount-type');
+let currentDiscountValue = document.getElementById('current-discount-value');
+let currentTotalTicketPrice = document.getElementById('current-total-price');
+console.log(currentName.innerHTML, currentAge.innerHTML, currentRouteDistance.innerHTML, currentTicketPrice.innerHTML, currentDiscountType.innerHTML, currentDiscountValue.innerHTML, currentTotalTicketPrice.innerHTML);
+
+//cambio info a schermo
+currentName.innerHTML = nameUser;
+currentAge.innerHTML = ageUser + ' anni';
+currentRouteDistance.innerHTML = routeDistance + ' km';
+currentTicketPrice.innerHTML = ticketPrice + ' €';
 
 // 3.2 valutare età passeggero e mostrare prezzo finale
 let totalTicketPrice;
 
 if (ageUser < 18) {
-    totalTicketPrice = parseFloat(ticketPrice * .80).toFixed(2)
+    totalTicketPrice = parseFloat(ticketPrice * .80).toFixed(2);
     totalTicketPrice = parseFloat(totalTicketPrice); // SE minorenne (età < 18), applicare 20% sconto
 
+    totalDiscount = parseFloat(ticketPrice * .20).toFixed(2);
+    totalDiscount = parseFloat(totalDiscount);
+
+    currentDiscountType.innerHTML = 'Young Promo';
+    currentDiscountValue.innerHTML = '- ' + totalDiscount + ' € (-20%)';
+    currentTotalTicketPrice.innerHTML = totalTicketPrice;
+
 } else if (ageUser > 65) {
-    totalTicketPrice = parseFloat(ticketPrice * .60).toFixed(2)
+    totalTicketPrice = parseFloat(ticketPrice * .60).toFixed(2);
     totalTicketPrice = parseFloat(totalTicketPrice);  // SE anziano (età > 65), applicare 40% sconto
+    
+    totalDiscount = parseFloat(ticketPrice * .40).toFixed(2);
+    totalDiscount = parseFloat(totalDiscount);
+
+    currentDiscountType.innerHTML = 'Senior Promo';
+    currentDiscountValue.innerHTML = '- ' + totalDiscount + ' € (-40%)';
+    currentTotalTicketPrice.innerHTML = totalTicketPrice;
 
 } else {
     totalTicketPrice = ticketPrice; // ALTRI CASI no sconto
+    currentTotalTicketPrice.innerHTML = totalTicketPrice + '€';
 }; 
 
 console.log(typeof totalTicketPrice, totalTicketPrice);
